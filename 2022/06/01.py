d9f011271da9fe58d699c6b22da6253cf401d474
Gurkirat Singh Khaira <62628582+khaira777@users.noreply.github.com>
2022-12-07 02:12:01 -0500

    2022 Day06

---
+++ 2022/06/01.py
@@ -0,0 +1,22 @@
+from collections import deque, Counter
+def isUniqueChars(string):
+    # Counting frequency
+    freq = Counter(string)
+    return len(freq) == len(string)
+
+with open('input.txt') as f:
+    lines = f.readlines()
+    packet = deque()
+    # print("before: ", packet)
+    for i,val in enumerate(lines[0]):
+        packet.append(val)
+        if len(packet) < 4:
+            # print("if: ", packet)
+            continue
+        else:
+            # print("else: ", packet)
+            if isUniqueChars(packet):
+                # print(''.join(packet))
+                print(i+1)
+                break
+            packet.popleft()
